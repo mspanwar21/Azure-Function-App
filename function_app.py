5fbf2cdc388fe58ac6bbc75ca080e3f529742c17
Mohit Singh Panwar <panw0011@algonquinlive.com>
2025-05-27 17:57:18 -0400

    Refactor HTTP trigger function and update host.json for application insights logging

--- function_app.py
+++ function_app.py
@@ -3,24 +3,23 @@ import logging
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
-@app.route(route="HttpExample")
-@app.queue_output(arg_name="msg", queue_name="outqueue", connection="AzureWebJobsStorage")
-def HttpExample(req: func.HttpRequest, msg: func.Out[func.QueueMessage]) -> func.HttpResponse:
-    logging.info('Processing HTTP request.')
+@app.route(route="http_trigger")
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
     if not name:
         try:
             req_body = req.get_json()
-            name = req_body.get('name')
         except ValueError:
             pass
+        else:
+            name = req_body.get('name')
 
     if name:
-        msg.set(name)
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body.",
-            status_code=200
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
         )
